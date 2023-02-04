4d93424667beceb805e5612c8d3b188368d1918d
Jaap Korthals Altes <jaapkorthalsaltes@gmail.com>
2023-02-04 16:14:53 +0100

    4.6.6: Floating glucose config

--- Common/src/wear/java/tk/glucodata/FloatingConfig.java
+++ Common/src/wear/java/tk/glucodata/FloatingConfig.java
@@ -16,53 +16,15 @@
 /*      You should have received a copy of the GNU General Public License            */
 /*      along with Juggluco. If not, see <https://www.gnu.org/licenses/>.            */
 /*                                                                                   */
-/*      Fri Jan 27 15:32:11 CET 2023                                                 */
+/*      Fri Jan 27 15:31:32 CET 2023                                                 */
 
 
 package tk.glucodata;
+import tk.glucodata.MainActivity;
 
-import android.app.Application;
 
-import static tk.glucodata.Natives.hasalarmloss;
-
-public class GlucoseAlarms extends SuperGlucoseAlarms {
-    final private static String LOG_ID="GlucoseAlarms";
-public GlucoseAlarms(Application context) {
-	super(context);
+public class FloatingConfig {
+	public static void show(MainActivity act) {
 	}
-
-public	void handlealarm() {
-		final long nu = System.currentTimeMillis();
-		long wastime = MyGattCallback.oldtime - showtime;
-		final long afterwait = waitmmsec() + wastime;
-		boolean shouldwake = Natives.shouldwakesender();
-		final long tryagain = nu + showtime;
-		if(afterwait > nu) {
-			Log.i(LOG_ID, "handlealarm notify");
-			Notify.onenot.oldnotification(wastime);
-			SuperGattCallback.previousglucose=null;
-			long nexttime = (!shouldwake || (afterwait < tryagain && hasalarmloss())) ? afterwait : tryagain;
-			LossOfSensorAlarm.setalarm(Applic.app, nexttime);
-		} else {
-			if (shouldwake)
-				LossOfSensorAlarm.setalarm(Applic.app, tryagain);
-			if (!saidloss) {
-				Log.i(LOG_ID, "handlealarm alarm");
-				long lasttime=Natives.lastglucosetime( );
-				if(lasttime!=0L)
-					wastime=lasttime;
-				if (hasalarmloss()) {
-					Notify.onenot.lossalarm(wastime);
-				}
-				saidloss = true;
-			}
-		}
-		if (shouldwake) {
-			MessageSender.sendwakestream();
-			Natives.wakestreamsender();
-			}
-	}
-
-
-
 }
+
